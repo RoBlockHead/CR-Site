6bbf5d63dd561c0cd6785294f2df1863f27d90ea
mastercoms <circuitsoft@openmailbox.org>
2015-04-05 18:03:15 -0400

    start website

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+$('#frc').hover(
+  function() {
+    $('#teamsb').css('background-image', "url('images/frcback.jpg')");
+  }, function() {
+    defaultBack();
+  }
+);
+$('#grits').hover(
+  function() {
+    $('#teamsb').css('background-image', "url('images/gritsback.jpg')");
+  }, function() {
+    defaultBack();
+  }
+);
+$('#best').hover(
+  function() {
+    $('#teamsb').css('background-image', "url('images/bestback.jpg')");
+  }, function() {
+    defaultBack();
+  }
+);
+$('#ftc').hover(
+  function() {
+    $('#teamsb').css('background-image', "url('images/ftcback.jpg')");
+  }, function() {
+    defaultBack();
+  }
+);
+
+function defaultBack() {
+  $('#teamsb').css('background-image', "url('images/circuitrunners.jpg')");
+};
